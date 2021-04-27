d0115aea95d49aa9af783947f65343bb8211c584
takashi oka <takashi.oka@gmail.com>
2021-04-27 18:17:46 +0900

    spec:models:task_spec.rb: revised wih examples

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,14 +7,16 @@ describe 'タスクモデル機能', type: :model do
         expect(task).not_to be_valid
       end
     end
-　　context 'タスクの詳細が空の場合' do
+    context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
+        task = Task.new(title: '失敗テスト2', content: '')
+        expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
+        task = Task.new(title: '成功テスト', content: '成功テスト')
+        expect(task).to be_valid
       end
     end
   end
