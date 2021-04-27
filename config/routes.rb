1029d8dec16c3a8da4ecdccad543f0d8b128d1df
takashi oka <takashi.oka@gmail.com>
2021-04-27 14:41:20 +0900

    task model: CRUD function revised

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :tasks
   root 'tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks do
+    collection do
+      post :confirm
+    end
+  end
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
