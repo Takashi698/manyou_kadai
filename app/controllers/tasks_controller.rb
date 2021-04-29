7618f90f75f150bfdc3fb1f69677066d1f46c903
takashi oka <takashi.oka@gmail.com>
2021-04-30 00:53:50 +0900

    task & status: search system complied

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,14 @@ class TasksController < ApplicationController
   def index
     if params[:sort_expired]
       @tasks = Task.order(expired_at: :desc)
+    elsif
+      if params[:search_title] != "" && params[:search_status] != ""
+        @tasks = Task.where("title LIKE ?", "%#{params[:search_title]}%").where(status_id: params[:search_status])
+      elsif params[:search_title] != "" && params[:search_status] == ""
+        @tasks = Task.where("title LIKE ?", "%#{params[:search_title]}%")
+      elsif  params[:search_title] == "" && params[:search_status] != ""
+        @tasks = Task.where(status_id: params[:search_status])
+      end
     else
       @tasks = Task.order(created_at: :desc)
     end
