1429314043bf9f9fe33e5ce50ef19ebd5908e7d6
takashi oka <takashi.oka@gmail.com>
2021-05-04 23:15:33 +0900

    task system spec function compiled, and fixed some failures

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,22 +1,22 @@
 FactoryBot.define do
   factory :task do
-    title { 'test_title' }
-    content { 'test_content' }
-    expired_at { '2021-03-03 03:33:33' }
+    title { 'task_title' }
+    content { 'task.title' }
+    expired_at { '2021-05-1 03:33:33' }
     status { 1 }
     priority { 1 }
   end
   factory :task2, class: "Task" do
     title { 'task2_title' }
-    content { 'task2_content' }
-    expired_at { '2021-05-15 03:33:33' }
+    content { 'Task.order(expired_at: :desc).first.title' }
+    expired_at { '2021-05-20 03:33:33' }
     status { 2 }
     priority { 2 }
   end
   factory :task3, class: "Task" do
     title { 'task3_title' }
-    content { 'task3_content' }
-    expired_at { '2021-03-21 03:33:33' }
+    content { 'Task.order(priority: :desc).first.title' }
+    expired_at { '2021-05-10 03:33:33' }
     status { 3 }
     priority { 3 }
   end
