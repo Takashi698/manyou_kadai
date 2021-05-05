5ee6904f87ec76cf99b585f6e5380c0c8adadbd6
takashi oka <takashi.oka@gmail.com>
2021-05-05 16:34:50 +0900

    label functioned to task controllers & views

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ Rails.application.routes.draw do
   end
   root 'tasks#index'
   resources :tasks do
-    collection do
-      post :confirm
-    end
-  end
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html```
