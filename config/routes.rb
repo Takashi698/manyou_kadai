c30dc7ff5efb4e620bc924725fb978c0772d07f9
takashi oka <takashi.oka@gmail.com>
2021-05-05 17:27:57 +0900

    revisions for system spec failures: Tasks view/form.html & indexhtml, task model:scope:seaarch_label

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :users
   end
   root 'tasks#index'
-  resources :tasks do
+  resources :tasks
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html```
